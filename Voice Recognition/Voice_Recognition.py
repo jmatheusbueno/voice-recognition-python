676fd245649c783da9bb8bc33a39ee34e11bf54f
João Matheus Bueno <49966257+jmatheusbueno@users.noreply.github.com>
2020-11-19 21:01:10 -0300

    Add files via upload

---
+++ Voice Recognition/Voice_Recognition.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import time as tm
+
+r = sr.Recognizer()
+with sr.Microphone() as font:
+
+    print('Hello, how can I help you?')
+    useraudio = 'Empty';
+    while useraudio != 'Não':
+        tm.sleep(2)
+        audio = r.listen(font)
+
+        try:
+            text = r.recognize_google(audio, language='pt-BR')
+            useraudio = text.capitalize()
+            if(useraudio == 'Sair' or useraudio == 'Não'):
+                print('You: ' + useraudio)
+                tm.sleep(1)
+                print('\nOk im leaving now.\n')
+                break
+
+            elif("NÃO" in useraudio.upper()):
+                print('You: ' + useraudio)
+                tm.sleep(1)
+                print('\nOk im leaving now.\n')
+                break
+
+            else:
+                print('You: ' + useraudio)
+                tm.sleep(1)
+                print('\nI can help with something else?')       
+                
+        except:
+            print('\nSorry I cant understand.\n')
+            break
+
+
+
+       
+
+
+
+
